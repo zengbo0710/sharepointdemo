0326a411be3d1ab131af00c2665dc08c33cba061
zengbo1228@gmail.com <zengbo1228@gmail.com@17c6fb2e-444b-6972-7fd6-08f60a78d29f>
2012-03-06 10:42:17 +0000

    upload file

--- trunk/SharePointDemo/java_src/com/sharepoint/demo/controller/TbMaintenanceController.java
+++ trunk/SharePointDemo/java_src/com/sharepoint/demo/controller/TbMaintenanceController.java
@@ -5,11 +5,13 @@
  * Since 2008 - 2012
  */
 
-
 package com.sharepoint.demo.controller;
 
-import java.util.Map;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,7 +25,6 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,25 +32,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import cn.org.rapid_framework.page.Page;
 import cn.org.rapid_framework.web.scope.Flash;
 
-import java.util.*;
-
-import javacommon.base.*;
-import javacommon.util.*;
-
-import cn.org.rapid_framework.util.*;
-import cn.org.rapid_framework.web.util.*;
-import cn.org.rapid_framework.page.*;
-import cn.org.rapid_framework.page.impl.*;
-
-import com.sharepoint.demo.model.*;
-import com.sharepoint.demo.dao.*;
-import com.sharepoint.demo.service.*;
+import com.sharepoint.demo.model.TbMaintenance;
+import com.sharepoint.demo.service.TbMaintenanceManager;
 import com.sharepoint.demo.util.MainConstants;
-import com.sharepoint.demo.vo.query.*;
+import com.sharepoint.demo.vo.query.TbMaintenanceQuery;
 
 /**
  * @author badqiu email:badqiu(a)gmail.com
@@ -59,27 +52,27 @@ import com.sharepoint.demo.vo.query.*;
 
 @Controller
 @RequestMapping("/tbmaintenance")
-public class TbMaintenanceController extends BaseRestSpringController<TbMaintenance,java.lang.Integer>{
-	//默认多列排序,example: username desc,createTime asc
-	protected static final String DEFAULT_SORT_COLUMNS = null; 
-	
+public class TbMaintenanceController extends BaseRestSpringController<TbMaintenance, java.lang.Integer> {
+	// 默认多列排序,example: username desc,createTime asc
+	protected static final String DEFAULT_SORT_COLUMNS = null;
+
 	private TbMaintenanceManager tbMaintenanceManager;
-	
+
 	private final String LIST_ACTION = "redirect:/tbmaintenance";
-	
-	/** 
+
+	/**
 	 * 增加setXXXX()方法,spring就可以通过autowire自动设置对象属性,注意大小写
 	 **/
 	public void setTbMaintenanceManager(TbMaintenanceManager manager) {
 		this.tbMaintenanceManager = manager;
 	}
-	
+
 	/** binder用于bean属性的设置 */
-	@InitBinder  
-	public void initBinder(WebDataBinder binder) {  
-	        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));  
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
 	}
-	   
+
 	/**
 	 * 增加了@ModelAttribute的方法可以在本controller方法调用前执行,可以存放一些共享变量,如枚举值,或是一些初始化操作
 	 */
@@ -89,80 +82,105 @@ public class TbMaintenanceController extends BaseRestSpringController<TbMaintena
 		model.addAttribute("status", MainConstants.statusMap);
 		model.addAttribute("type", MainConstants.typeMap);
 	}
-	
+
 	/** 列表 */
 	@RequestMapping
-	public String index(ModelMap model,TbMaintenanceQuery query,HttpServletRequest request,HttpServletResponse response) {
+	public String index(ModelMap model, TbMaintenanceQuery query, HttpServletRequest request, HttpServletResponse response) {
 		Page page = this.tbMaintenanceManager.findPage(query);
-		
+
 		model.addAllAttributes(toModelMap(page, query));
 		return "/tbmaintenance/index";
 	}
-	
+
 	/** 显示 */
-	@RequestMapping(value="/{id}")
-	public String show(ModelMap model,@PathVariable java.lang.Integer id) throws Exception {
-		TbMaintenance tbMaintenance = (TbMaintenance)tbMaintenanceManager.getById(id);
-		model.addAttribute("tbMaintenance",tbMaintenance);
+	@RequestMapping(value = "/{id}")
+	public String show(ModelMap model, @PathVariable java.lang.Integer id) throws Exception {
+		TbMaintenance tbMaintenance = (TbMaintenance) tbMaintenanceManager.getById(id);
+		model.addAttribute("tbMaintenance", tbMaintenance);
 		return "/tbmaintenance/show";
 	}
 
 	/** 进入新增 */
-	@RequestMapping(value="/new")
-	public String _new(ModelMap model,TbMaintenance tbMaintenance,HttpServletRequest request,HttpServletResponse response) throws Exception {
-		model.addAttribute("tbMaintenance",tbMaintenance);
+	@RequestMapping(value = "/new")
+	public String _new(ModelMap model, TbMaintenance tbMaintenance, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		model.addAttribute("tbMaintenance", tbMaintenance);
 		return "/tbmaintenance/new";
 	}
-	
-	/** 保存新增,@Valid标注spirng在绑定对象时自动为我们验证对象属性并存放errors在BindingResult  */
-	@RequestMapping(method=RequestMethod.POST)
-	public String create(ModelMap model,@Valid TbMaintenance tbMaintenance,BindingResult errors,HttpServletRequest request,HttpServletResponse response) throws Exception {
-		if(errors.hasErrors()) {
-			return  "/tbmaintenance/new";
+
+	/** 保存新增,@Valid标注spirng在绑定对象时自动为我们验证对象属性并存放errors在BindingResult */
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(@RequestParam(value = "file", required = false) MultipartFile uploadFile, ModelMap model, @Valid TbMaintenance tbMaintenance, BindingResult errors, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (errors.hasErrors()) {
+			return "/tbmaintenance/new";
 		}
-		
+		try {
+			String fileName = uploadFile.getOriginalFilename();  
+			InputStream is = uploadFile.getInputStream();
+			String uploadFilePath = MainConstants.getUploadPath(request.getLocalPort());
+			//如果服务器已经存在和上传文件同名的文件，则输出提示信息
+			File tempFile = new File(uploadFilePath + fileName);
+			if (tempFile.exists()) {
+				boolean delResult = tempFile.delete();
+				System.out.println("删除已存在的文件：" + delResult);
+			}
+			//开始保存文件到服务器
+			if (!fileName.equals("")) {
+				FileOutputStream fos = new FileOutputStream(uploadFilePath + fileName);
+				byte[] buffer = new byte[8192]; // 每次读8K字节
+				int count = 0;
+				//开始读取上传文件的字节，并将其输出到服务端的上传文件输出流中
+				while ((count = is.read(buffer)) > 0) {
+					fos.write(buffer, 0, count); // 向服务端文件写入字节流
+				}
+				fos.close(); //关闭FileOutputStream对象
+				is.close(); //InputStream对象
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		tbMaintenanceManager.save(tbMaintenance);
-		Flash.current().success(CREATED_SUCCESS); //存放在Flash中的数据,在下一次http请求中仍然可以读取数据,error()用于显示错误消息
+		Flash.current().success(CREATED_SUCCESS); // 存放在Flash中的数据,在下一次http请求中仍然可以读取数据,error()用于显示错误消息
 		return LIST_ACTION;
 	}
-	
+
 	/** 编辑 */
-	@RequestMapping(value="/{id}/edit")
-	public String edit(ModelMap model,@PathVariable java.lang.Integer id) throws Exception {
-		TbMaintenance tbMaintenance = (TbMaintenance)tbMaintenanceManager.getById(id);
-		model.addAttribute("tbMaintenance",tbMaintenance);
+	@RequestMapping(value = "/{id}/edit")
+	public String edit(ModelMap model, @PathVariable java.lang.Integer id) throws Exception {
+		TbMaintenance tbMaintenance = (TbMaintenance) tbMaintenanceManager.getById(id);
+		model.addAttribute("tbMaintenance", tbMaintenance);
 		return "/tbmaintenance/edit";
 	}
-	
-	/** 保存更新,@Valid标注spirng在绑定对象时自动为我们验证对象属性并存放errors在BindingResult  */
-	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
-	public String update(ModelMap model,@PathVariable java.lang.Integer id,@Valid TbMaintenance tbMaintenance,BindingResult errors,HttpServletRequest request,HttpServletResponse response) throws Exception {
-		if(errors.hasErrors()) {
+
+	/** 保存更新,@Valid标注spirng在绑定对象时自动为我们验证对象属性并存放errors在BindingResult */
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public String update(ModelMap model, @PathVariable java.lang.Integer id, @Valid TbMaintenance tbMaintenance, BindingResult errors, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (errors.hasErrors()) {
 			return "/tbmaintenance/edit";
 		}
-		
+
 		tbMaintenanceManager.update(tbMaintenance);
 		Flash.current().success(UPDATE_SUCCESS);
 		return LIST_ACTION;
 	}
-	
+
 	/** 删除 */
-	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
-	public String delete(ModelMap model,@PathVariable java.lang.Integer id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public String delete(ModelMap model, @PathVariable java.lang.Integer id) {
 		tbMaintenanceManager.removeById(id);
 		Flash.current().success(DELETE_SUCCESS);
 		return LIST_ACTION;
 	}
 
 	/** 批量删除 */
-	@RequestMapping(method=RequestMethod.DELETE)
-	public String batchDelete(ModelMap model,@RequestParam("items") java.lang.Integer[] items) {
-		for(int i = 0; i < items.length; i++) {
+	@RequestMapping(method = RequestMethod.DELETE)
+	public String batchDelete(ModelMap model, @RequestParam("items") java.lang.Integer[] items) {
+		for (int i = 0; i < items.length; i++) {
 			tbMaintenanceManager.removeById(items[i]);
 		}
 		Flash.current().success(DELETE_SUCCESS);
 		return LIST_ACTION;
 	}
-	
-}
 
+}
--- trunk/SharePointDemo/java_src/com/sharepoint/demo/util/MainConstants.java
+++ trunk/SharePointDemo/java_src/com/sharepoint/demo/util/MainConstants.java
@@ -1,10 +1,15 @@
 package com.sharepoint.demo.util;
 
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class MainConstants {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+public class MainConstants {
 	public static final Integer NOT_STARTED_KEY = 1;
 	public static final Integer IN_PROGRESS_KEY = 2;
 	public static final Integer COMPLETED_KEY = 3;
@@ -15,24 +20,58 @@ public class MainConstants {
 	public static final String COMPLETED_VALUE = "Completed";
 	public static final String DEFERRED_VALUE = "Deferred";
 	public static final String WAITING_ON_SOMEONE_ELSE_VALUE = "Waiting on someone else";
-	
+
 	public static final Integer TYPE1_KEY = 1;
 	public static final Integer TYPE2_KEY = 2;
 	public static final String TYPE1_VALUE = "One";
 	public static final String TYPE2_VALUE = "Two";
 	
-	
-	public static Map<Integer,String> statusMap = new LinkedHashMap<Integer,String>();
-	public static Map<Integer,String> typeMap = new LinkedHashMap<Integer,String>();
-	
-	static{
+	public static final String LOCAL_PATH = "/SharePointDemo/upload/";
+
+	public static Map<Integer, String> statusMap = new LinkedHashMap<Integer, String>();
+	public static Map<Integer, String> typeMap = new LinkedHashMap<Integer, String>();
+
+	static {
 		statusMap.put(NOT_STARTED_KEY, NOT_STARTED_VALUE);
 		statusMap.put(IN_PROGRESS_KEY, IN_PROGRESS_VALUE);
 		statusMap.put(COMPLETED_KEY, COMPLETED_VALUE);
 		statusMap.put(DEFERRED_KEY, DEFERRED_VALUE);
 		statusMap.put(WAITING_ON_SOMEONE_ELSE_KEY, WAITING_ON_SOMEONE_ELSE_VALUE);
-		
-		typeMap.put(TYPE1_KEY,TYPE1_VALUE);
-		typeMap.put(TYPE2_KEY,TYPE2_VALUE);
+
+		typeMap.put(TYPE1_KEY, TYPE1_VALUE);
+		typeMap.put(TYPE2_KEY, TYPE2_VALUE);
+	}
+
+	/*
+	 * @return true---是Windows操作系统
+	 */
+	public static boolean isWindowsOS() {
+		boolean isWindowsOS = false;
+		String osName = System.getProperty("os.name");
+		if (osName.toLowerCase().indexOf("windows") > -1) {
+			isWindowsOS = true;
+		}
+		return isWindowsOS;
+	}
+
+	public static String getUploadPath(int port) {
+		String ip = "";
+		boolean isWindows = isWindowsOS();
+		try {
+			if (isWindows) {
+				InetAddress address = InetAddress.getLocalHost();
+				ip = address.getHostAddress();
+			} else {
+				ip = SystemTools.getLinuxIP();
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String savePath = "http://" + ip + ":" + port + LOCAL_PATH; // 保存到数据库的地址,图片上传到服务器端
+		return savePath;
 	}
 }
